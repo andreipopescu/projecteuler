97d289a89a91168cfe6b9d6a9173676a3c3e01a0
andrei <andrei_popescu87@hotmail.com>
2017-11-03 21:29:31 +0200

    speedup primes generator

--- utils/__init__.py
+++ utils/__init__.py
@@ -28,13 +28,18 @@ def primes(stop_func):
     >>> list(primes(lambda x: x <= no_of_primes))
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     """
-    prime, count = 2, 1
+    prime, count = 1, 1
     while stop_func(count):
-        for d in range(2, prime):
-            if prime % d == 0:
-                break
-        else:
+        if prime in {1, 2}:
+            prime += 1
             yield prime
             count += 1
-        prime += 1
+        else:
+            prime += 2
+            for d in range(2, prime):
+                if prime % d == 0:
+                    break
+            else:
+                yield prime
+                count += 1
 
